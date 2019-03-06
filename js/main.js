027ba150fbbc19e9f966efd6282d46e3004db029
Dmitry <ddbtav@gmail.com>
2019-03-06 12:03:05 -0500

    Pulling shows list work

--- js/main.js
+++ js/main.js
@@ -10,52 +10,50 @@ $(document).ready(
 
             console.log("Input was: " + search_string);
 
-            $.ajax('http://api.tvmaze.com/singlesearch/shows?q=' + search_string + '&embed=episodes', {
-                method: "GET",
-                dataType: "json"
-            })
 
-            //After the data comes back, use this function
-                .done(
-                    function (data) {
-                        //Add the name
-                        $('#name').append(data.name);
-                        //Add the episodes
-                        data._embedded.episodes.forEach(function (episode) {
-                            $('#episodeList').append('<tr>'+
-                                '<td>' + episode.season + '</td>' +
-                                '<td>' + episode.number + '</td>' +
-                                '<td>' + episode.name + '</td>' +
-                                '<td>' + episode.summary + '</td>' +
-                                +' </tr>')
+
+                $.ajax('http://api.tvmaze.com/search/shows?q=' + search_string, {
+                    method: "GET",
+                    dataType: "json"
+                })
+
+                //After the data comes back, use this function
+                    .done(
+                        function (data) {
+                            //Add the name
+                            // $('#name').append(data.name);
+                            //Add the episodes
+                            data.forEach(function (result) {
+                                $('#showsList').append('<tr>'+
+                                    '<td>' + result.show.name + '</td>' +
+                                    '<td>' + result.show.genres[0] + '</td>' +
+                                    '<td>' + result.show.officialSite + '</td>' +
+                                    +' </tr>')
+                            })
                         })
-                    })
+
+            // $.ajax('http://api.tvmaze.com/singlesearch/shows?q=' + search_string + '&embed=episodes', {
+            //     method: "GET",
+            //     dataType: "json"
+            // })
+            //
+            // //After the data comes back, use this function
+            //     .done(
+            //         function (data) {
+            //             //Add the name
+            //             $('#name').append(data.name);
+            //             //Add the episodes
+            //             data._embedded.episodes.forEach(function (episode) {
+            //                 $('#episodeList').append('<tr>'+
+            //                     '<td>' + episode.season + '</td>' +
+            //                     '<td>' + episode.number + '</td>' +
+            //                     '<td>' + episode.name + '</td>' +
+            //                     '<td>' + episode.summary + '</td>' +
+            //                     +' </tr>')
+            //             })
+            //         })
 
         });
-
-        // let search_string = $('input#search_text').val();
-        // console.log(search_string);
-
-
-
-        // let search_string = "test";
-
-
-        // let test_str = "City";
-
-        // var search_string = $('input#search_text').val();
-        // console.log(search_string);
-
-        //Make the AJAX call
-        // $.ajax('http://api.tvmaze.com/singlesearch/shows?q=the+magicians&embed=episodes', {
-        //     method: "GET",
-        //     dataType: "json"
-        // })
-        // $.ajax('http://api.tvmaze.com/singlesearch/shows?q=' + test_str + '&embed=episodes', {
-        //     method: "GET",
-        //     dataType: "json"
-        // })
-
 
     }
 )
