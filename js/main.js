e7df95cfbd1f827adf3463c20cf434842940399d
Dmitry <ddbtav@gmail.com>
2019-03-06 11:24:03 -0500

    First try - with error - before code move inside on click

--- js/main.js
+++ js/main.js
@@ -2,11 +2,42 @@
 $(document).ready(
     //The function that does the stuff.
     function () {
+        $('input#submit_btn').click(function(event){
+            event.preventDefault();
+            console.log("Submit button clicked");
+            let search_string = $('input#search_text').val();
+            console.log(search_string);
+
+        });
+
+        // let search_string = $('input#search_text').val();
+        // console.log(search_string);
+
+        console.log("Input was: " + search_string);
+
+        // let search_string = "test";
+
+
+        // let test_str = "City";
+
+        // var search_string = $('input#search_text').val();
+        // console.log(search_string);
+
         //Make the AJAX call
-        $.ajax('http://api.tvmaze.com/singlesearch/shows?q=the+magicians&embed=episodes', {
+        // $.ajax('http://api.tvmaze.com/singlesearch/shows?q=the+magicians&embed=episodes', {
+        //     method: "GET",
+        //     dataType: "json"
+        // })
+        // $.ajax('http://api.tvmaze.com/singlesearch/shows?q=' + test_str + '&embed=episodes', {
+        //     method: "GET",
+        //     dataType: "json"
+        // })
+
+        $.ajax('http://api.tvmaze.com/singlesearch/shows?q=' + search_string + '&embed=episodes', {
             method: "GET",
             dataType: "json"
         })
+
         //After the data comes back, use this function
             .done(
                 function (data) {
